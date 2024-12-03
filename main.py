094de63168567c9542653294ca73abb5f69a583e
sakura <946788898@qq.com>
2024-12-03 21:53:18 +0800

    添加微博热搜跟踪

---
+++ main.py
@@ -0,0 +1,28 @@
+
+import time
+from model import *
+
+def main():
+    check_path(log_path)
+    to_email, from_email, password = get_config(config_path=config_path)
+    realtime_hot_str,realtime_hot_top = hot_search(url)
+    latest_file_path=get_latest_file(log_path)
+    if latest_file_path is None :
+        compare_content(log_path, realtime_hot_str, to_email, from_email, password)
+    else:
+        with open(latest_file_path, 'r') as f:
+            latest_file_first_line = f.readline().strip()
+    if realtime_hot_top != latest_file_first_line:
+        print("Hot search has changed")
+        compare_content(log_path, realtime_hot_str, to_email, from_email, password)
+    else:
+        print("Hot search has not changed")
+    time.sleep(60) # 10 seconds
+
+if __name__ == '__main__':
+    url = 'https://weibo.com/ajax/side/hotSearch'
+    log_path='./log'
+    config_path='config.json'
+    while True:
+        main()
+    pass
---
+++ model.py
@@ -0,0 +1,78 @@
+import requests
+import pandas as pd
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import json
+from datetime import datetime
+import os
+
+def hot_search(url):
+    
+    response = requests.get(url)
+    if response.status_code != 200:
+        return None
+    results = response.json()['data']
+    realtime_hot = results['realtime']
+    realtime_hot_df = pd.DataFrame(realtime_hot)['word']
+    realtime_hot_str=realtime_hot_df.to_string(index=True)
+    realtime_hot_top = realtime_hot_str.splitlines()[0]
+    return realtime_hot_str,realtime_hot_top
+
+def send_email(email_subject, email_body, to_email_list,from_email,password):
+    for i in range(len(to_email_list)):
+        to_email=to_email_list[i]
+        msg = MIMEMultipart()
+        msg['From'] = from_email
+        msg['To'] = to_email
+        msg['Subject'] = email_subject
+
+        msg.attach(MIMEText(email_body, 'plain'))
+
+        try:
+            server = smtplib.SMTP('smtp.163.com', 25)
+            server.ehlo()
+            server.starttls()
+            server.login(from_email, password)
+            text = msg.as_string()
+            server.sendmail(from_email, to_email, text)
+            server.quit()
+            print("Email sent successfully")
+        except Exception as e:
+            print(f"Failed to send email: {e}")
+
+def check_path(file_path):
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+
+def get_config(config_path):
+    config_data = json.load(open('config.json'))
+    to_email = config_data['target']
+    from_email = config_data['from_email']
+    password = config_data['password']
+    return to_email, from_email, password
+
+def get_latest_file(file_path):
+    files=os.listdir(file_path)
+    if len(files)==0:
+        return None
+    latest_file=None
+    latest_time=None
+    for file in files:
+        try:
+            file_time = datetime.strptime(file, '%Y-%m-%d-%H-%M_weibo_hot_search.txt')
+            if latest_time is None or file_time > latest_time:
+                latest_time = file_time
+                latest_file = file
+        except ValueError:
+            continue
+    latest_file_path=os.path.join(file_path,latest_file)
+    return latest_file_path
+
+def compare_content(log_path,realtime_hot_str,to_email,from_email,password):
+        now = datetime.now().strftime('%Y-%m-%d-%H-%M')
+        output_path_name=f'{now}_weibo_hot_search.txt'
+        output_path=os.path.join(log_path,output_path_name)
+        with open(output_path, 'w') as f:
+            f.write(realtime_hot_str)
+        send_email(f'微博热搜榜({now})', realtime_hot_str, to_email,from_email,password)
